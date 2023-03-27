4f1e1bcf8fc57ce6237d6c9f89092cfde5653c13
swaroop chandra <swaroop.chandra@taskmo.com>
2023-03-28 03:10:12 +0530

    color adjustment

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -54,7 +54,7 @@ export default function Navigation() {
         <li>
           <Link href="/lets_talk">
             <button
-              className="transition p-3 ease-in-out delay-150 text-slate-600 hover:scale-110 duration-300 active:scale-100 active:text-black hover:text-black text-base bg-neutral-100 text-sm rounded flex flex-row items-center justify-self-center active:text-rose-700 active:bg-rose-700"
+              className="transition p-3 ease-in-out delay-150 text-slate-600 hover:scale-110 duration-300 active:scale-100 active:text-black hover:text-black text-base bg-neutral-200 text-sm rounded flex flex-row items-center justify-self-center active:text-rose-700 active:bg-rose-700"
               style={{ width: "145px", height: "32px" }}
             >
               Let’s Talk
